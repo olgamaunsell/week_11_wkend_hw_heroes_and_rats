7e44115d1af7997a25441ad84c5b951eaf5b9c75
Olga Maunsell <olgamaunsell@yahoo.co.uk>
2018-02-17 12:12:00 +0000

    5 tests passed

--- hero.js
+++ hero.js
@@ -4,6 +4,8 @@ const Hero = function(name, favFood){
   this.health = 0;
 }
 
-
+Hero.prototype.talk = function(){
+  return `Hello my name is ${this.name} !`;
+}
 
 module.exports = Hero;
--- specs/hero_spec.js
+++ specs/hero_spec.js
@@ -9,25 +9,35 @@ describe('Hero tests', function (){
       hero1 = new Hero("Tom", "Cheese");
   })
 
-  it('should have a name', function(){
+  it("should have a name", function(){
     assert.strictEqual(hero1.name, "Tom");
+  });
+
+  it("should have a favourite food", function(){
+    assert.strictEqual(hero1.favFood, "Cheese");
+  });
+
+  it("should talk saying it's name", function(){
+    const talk = hero1.talk();
+    assert.strictEqual(talk, "Hello my name is Tom !");
   });
 
   it('should have a favourite food', function(){
     assert.strictEqual(hero1.favFood, "Cheese");
   });
-  
+
   describe('Health tests', function (){
     it('should have health starting at zero', function(){
       assert.strictEqual(hero1.health, 0);
     });
 
-
   })
+
+
+
 })
 
 
-// A Hero has health
-// A Hero has a favourite food
+
 // A Hero can talk saying their name
 // A Hero has a collection of tasks to complete, which leads nicely to -
